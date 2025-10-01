2c21a5d6ab8e54cde4f13250698a6f6b8ef506db
Mike Katholnig <mk3819@gmail.com>
2025-10-01 10:22:37 +0100

    fix(trailer): big trailer now has 5 rows x 4 cells

--- src/pages/Trailer.tsx
+++ src/pages/Trailer.tsx
@@ -26,12 +26,13 @@ function createLayout(mode: 'small' | 'big') {
   }
   return {
     columns: 4,
-    rows: 4,
+    rows: 5,
     cellIds: [
       'b-0-0','b-1-0','b-2-0','b-3-0',
       'b-0-1','b-1-1','b-2-1','b-3-1',
       'b-0-2','b-1-2','b-2-2','b-3-2',
       'b-0-3','b-1-3','b-2-3','b-3-3',
+      'b-0-4','b-1-4','b-2-4','b-3-4',
       'b-base',
     ],
   }
